eeea10521c1851ea1c65b19f587d5b4605095794
Charles Bergeron <chuck.bergeron@gmail.com>
2020-07-30 15:54:20 -0700

    * null out players timelock timestamp if they re-deposit all their timelocked funds back into the pool

--- src/mappingForPrizePool.ts
+++ src/mappingForPrizePool.ts
@@ -1,5 +1,6 @@
 import { Address, BigInt, log } from '@graphprotocol/graph-ts'
 import {
+  Player,
   PrizeStrategy,
   PrizePool,
 } from '../generated/schema'
@@ -9,8 +10,10 @@ import {
 
 import {
   PrizePool as PrizePoolContract,
+  CapturedAward,
+  TimelockDeposited,
   Deposited,
-  // Awarded,
+  Awarded,
   AwardedExternal,
   InstantWithdrawal,
   TimelockedWithdrawal,
@@ -23,6 +26,69 @@ import { loadOrCreatePlayer } from './helpers/loadOrCreatePlayer'
 
 const ZERO = BigInt.fromI32(0)
 const ONE = BigInt.fromI32(1)
+
+
+function updateTotalTicketSupply(_prizePool: PrizePool): void {
+  const _prizeStrategy = PrizeStrategy.load(_prizePool.prizeStrategy)
+
+  const ticket = _prizeStrategy.ticket
+
+  // TODO: handle edge cases like if token isn't ticket
+  // event.params.token
+  const boundTicket = ERC20Contract.bind(
+    Address.fromString(ticket.toHexString())
+  )
+  _prizePool.totalSupply = boundTicket.totalSupply()
+
+  _prizePool.save()
+}
+
+function decrementPlayerCount(_prizePool: PrizePool, _player: Player): void {
+  if (_player.balance.equals(ZERO)) {
+    log.warning('in instantWithdrawal if!', [])
+    _prizePool.playerCount = _prizePool.playerCount.minus(ONE)
+
+    _prizePool.save()
+  }
+}
+
+function incrementPlayerCount(_prizePool: PrizePool, playersCachedBalance: BigInt): void {
+  if (playersCachedBalance.equals(ZERO)) {
+    _prizePool.playerCount = _prizePool.playerCount.plus(ONE)
+
+    _prizePool.save()
+  }
+}
+
+function decrementPlayerBalance(_player: Player, amount: BigInt): void {
+  _player.balance = _player.balance.minus(amount)
+  _player.save()
+}
+
+function incrementPlayerBalance(_player: Player, amount: BigInt): void {
+  _player.balance = _player.balance.plus(amount)
+  _player.save()
+}
+
+function decrementPlayerTimelockedBalance(_player: Player, amount: BigInt): void {
+  _player.timelockedBalance = _player.timelockedBalance.minus(amount)
+  _player.save()
+}
+
+function incrementPlayerTimelockedBalance(_player: Player, amount: BigInt): void {
+  _player.timelockedBalance = _player.timelockedBalance.plus(amount)
+  _player.save()
+}
+
+export function handleCapturedAward(event: CapturedAward): void {
+  // TODO!
+  log.warning('no-op!', [])
+}
+
+export function handleAwarded(event: Awarded): void {
+  // TODO!
+  log.warning('no-op!', [])
+}
 
 export function handleAwardedExternal(event: AwardedExternal): void {
   // This is emitted when external rewards (other tokens, nfts, etc)
@@ -38,96 +104,110 @@ export function handleDeposited(event: Deposited): void {
   const token = event.params.token
 
   const ticketAddress = Address.fromString(ticket.toHexString())
-
   const ticketIsToken = (token.equals(ticketAddress))
   
-  if (token.equals(ticketAddress)) {
+  if (ticketIsToken) {
     const _player = loadOrCreatePlayer(
       Address.fromString(event.address.toHex()),
       event.params.to
     )
     
-    log.warning('supply _player.balance: {}', [_player.balance.toString()])
+    const playersCachedBalance = _player.balance
+    incrementPlayerCount(_prizePool as PrizePool, playersCachedBalance)
 
-    if (_player.balance.equals(ZERO)) {
-      log.warning('in supply if!', [])
-      log.warning('_prizePool.playerCount: {}', [_prizePool.playerCount.toString()])
-      _prizePool.playerCount = _prizePool.playerCount.plus(ONE)
-      log.warning('_prizePool.playerCount: {}', [_prizePool.playerCount.toString()])
-    }
-
-    const boundTicket = ERC20Contract.bind(
-      Address.fromString(ticket.toHexString())
-    )
-    _prizePool.totalSupply = boundTicket.totalSupply()
-    _prizePool.save()
+    updateTotalTicketSupply(_prizePool as PrizePool)
 
     _player.address = event.params.to
     _player.prizePool = event.address.toHex()
-    _player.balance = _player.balance.plus(event.params.amount)
+
+    incrementPlayerBalance(_player, event.params.amount)
     // _player.shares = _player.shares.plus(event.params.shares)
+
     _player.save()
   } else if (token === _prizeStrategy.sponsorship) {
     // FILL IN SPONSORSHIP WHAT HAPPENS HERE
 
+    // _sponsor.save()
   }
+
+  _prizePool.save()
 }
 
 export function handleInstantWithdrawal(event: InstantWithdrawal): void {
   const _prizePool = PrizePool.load(event.address.toHex())
-  const _prizeStrategy = PrizeStrategy.load(_prizePool.prizeStrategy)
   const _player = loadOrCreatePlayer(
     Address.fromString(event.address.toHex()),
     event.params.from
   )
 
-  _player.balance = _player.balance.minus(event.params.amount)
+  decrementPlayerBalance(_player, event.params.amount)
   // _player.shares = _player.shares.minus(event.params.shares)
 
-  const boundTicket = ERC20Contract.bind(
-    Address.fromString(_prizeStrategy.ticket.toHex())
-  )
-  _prizePool.totalSupply = boundTicket.totalSupply()
+  updateTotalTicketSupply(_prizePool as PrizePool)
 
+  decrementPlayerCount(_prizePool as PrizePool, _player)
   log.warning('instantWithdrawal _player.balance: {}', [_player.balance.toString()])
-  if (_player.balance.equals(ZERO)) {
-    log.warning('in instantWithdrawal if!', [])
-    _prizePool.playerCount = _prizePool.playerCount.minus(ONE)
-  }
 
   _prizePool.save()
   _player.save()
 }
 
 export function handleTimelockedWithdrawal(event: TimelockedWithdrawal): void {
+  const _prizePool = PrizePool.load(event.address.toHex())
+
   // const timelock = Timelock.load(event.address.toHex())
-  const player = loadOrCreatePlayer(
-    Address.fromString(event.address.toHex()),
-    event.params.from
-  )
-
-  // increment or decrement prize pool player count if balance is now 0
-
-  // This may need to be an association of many timelocked balances per player
-  player.timelockedBalance = player.timelockedBalance.plus(event.params.amount)
-  player.unlockTimestamp = event.params.unlockTimestamp
-
-  player.save()
-}
-
-export function handleTimelockedWithdrawalSwept(event: TimelockedWithdrawalSwept): void {
-  // const _prizePool = PrizePool.load(event.address.toHex())
   const _player = loadOrCreatePlayer(
     Address.fromString(event.address.toHex()),
     event.params.from
   )
 
-  _player.timelockedBalance = _player.timelockedBalance.minus(event.params.amount)
-  // _player.unlockTimestamp = event.params.unlockTimestamp ?
+  decrementPlayerCount(_prizePool as PrizePool, _player)
+  
+  decrementPlayerBalance(_player, event.params.amount)
+  incrementPlayerTimelockedBalance(_player, event.params.amount)
 
-  // increment or decrement prize pool player count if balance is now 0
+  updateTotalTicketSupply(_prizePool as PrizePool)
+
+  // This may need to be an association of many timelocked balances per player
+  _player.unlockTimestamp = event.params.unlockTimestamp
+
+  _player.save()
+  _prizePool.save()
+}
+
+export function handleTimelockedWithdrawalSwept(event: TimelockedWithdrawalSwept): void {
+  const _player = loadOrCreatePlayer(
+    Address.fromString(event.address.toHex()),
+    event.params.from
+  )
+
+  decrementPlayerTimelockedBalance(_player, event.params.amount)
+  _player.unlockTimestamp = null
 
   _player.save()
 }
 
+// This happens when a player deposits some of their timelocked funds
+// back into the pool
+export function handleTimelockDeposited(event: TimelockDeposited): void {
+  const _prizePool = PrizePool.load(event.address.toHex())
+  const _player = loadOrCreatePlayer(
+    Address.fromString(event.address.toHex()),
+    event.params.to
+  )
 
+  const playersCachedBalance = _player.balance
+  incrementPlayerCount(_prizePool as PrizePool, playersCachedBalance)
+
+  updateTotalTicketSupply(_prizePool as PrizePool)
+
+  decrementPlayerTimelockedBalance(_player, event.params.amount)
+  incrementPlayerBalance(_player, event.params.amount)
+
+  if (_player.timelockedBalance.equals(ZERO)) {
+    _player.unlockTimestamp = null
+  }
+
+  _player.save()
+  _prizePool.save()
+}
