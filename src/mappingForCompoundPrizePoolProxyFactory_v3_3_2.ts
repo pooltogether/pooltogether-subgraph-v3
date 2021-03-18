d2863e62bdc5b158602e8a9ff702f8e7f2e662ed
Aodhgan <aodhgan@pooltogether.com>
2021-03-18 13:09:35 -0700

    uploaded to rinkeby and mainnnet

--- src/mappingForCompoundPrizePoolProxyFactory_v3_3_2.ts
+++ src/mappingForCompoundPrizePoolProxyFactory_v3_3_2.ts
@@ -1,19 +1,17 @@
 import {
   ProxyCreated,
-} from '../generated/CompoundPrizePoolProxyFactory_v3_1_0/CompoundPrizePoolProxyFactory_v3_1_0'
+} from '../generated/CompoundPrizePoolProxyFactory_v3_3_2/CompoundPrizePoolProxyFactory_v3_3_2'
 
 import {
-  PrizePool_v3 as PrizePoolV3Template,
-  PrizePool_v3_1_0 as PrizePoolTemplate,
   PrizePool_v3_3_2,
-  CompoundPrizePool as CompoundPrizePoolTemplate,
+  CompoundPrizePool as CompoundPrizePoolTemplate, // cannot label v3_3_2 because is coming from node_modules
 } from '../generated/templates'
 
 export function handleProxyCreated(event: ProxyCreated): void {
   // Start listening for events from the dynamically generated contract
-  PrizePoolV3Template.create(event.params.proxy)
-  PrizePoolTemplate.create(event.params.proxy)
   PrizePool_v3_3_2.create(event.params.proxy)
+  
+  
   CompoundPrizePoolTemplate.create(event.params.proxy)
 
 }
